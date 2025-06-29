438746c65afc91fc7449d47f08ec2ffccfeaf759
flyingwithgm <georgenuyekpe@gmail.com>
2025-06-29 17:48:03 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Mobile Navbar Toggle
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
